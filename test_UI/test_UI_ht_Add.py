a20f846734cf16c39555e2dc82802e26ab214eab
user <yx20251215@outlook.com>
2026-03-07 01:42:24 +0800

    web自动化数据驱动

--- test_UI/test_UI_ht_Add.py
+++ test_UI/test_UI_ht_Add.py
@@ -1,17 +1,18 @@
 import os
 import uuid
-
 import allure
 import pytest
 
 
 from playwright.sync_api import sync_playwright, expect
+from utils.Yaml_Util import yaml1
+
 
 
 @pytest.fixture(autouse=True,scope="class")
 def login():
     with sync_playwright() as p:
-        a = p.chromium.launch()
+        a = p.chromium.launch(headless=False)
         b = a.new_context()
         page = b.new_page()
         page.goto("https://kdtx-test.itheima.net/#/login")
@@ -25,153 +26,66 @@ def login():
         yield page
 
 
+a = yaml1('UI_add').data2()
+
 @allure.epic('客达天下')
-@allure.feature('添加合同')
+@allure.feature('UI_添加合同')
 class Test_UI_ht_add:
-    te = uuid.uuid4().hex[:9]
-    path = os.path.dirname(__file__)
-    pdf = path + os.sep + '1.pdf'
-
-
-    @allure.title('添加合同成功')
-    def test_add1(self,login):
+    @pytest.mark.flaky(reruns=1, reruns_delay=2)
+    @pytest.mark.parametrize('title,data,x',a)
+    def test_add5(self,login,title,data,x):
         page = login
+        allure.dynamic.title(title)
         page.reload()
-
-        with allure.step('1、进入添加合同页面'):
+        with allure.step('进入添加合同页面'):
             page.get_by_role("button", name="添加合同").click()
-        with allure.step('2、输入姓名、手机号、合同编号'):
-            page.get_by_role("dialog", name="添加合同").get_by_placeholder("请输入客户姓名").fill('张三')
-            page.get_by_role("dialog", name="添加合同").get_by_placeholder("请输入客户手机号").fill('13212341234')
-            page.get_by_role("dialog", name="添加合同").get_by_placeholder("请输入合同编号").fill(self.te)
-        with allure.step('3、选择学科与课程'):
-            page.get_by_role("dialog", name="添加合同").get_by_placeholder("请选择购买学科").click()
-            page.get_by_role("listitem").filter(has_text="Java").click()
-            page.get_by_role("dialog", name="添加合同").get_by_placeholder("请选择购买课程").click()
-            page.locator('xpath=/html/body/div[5]/div[1]/div[1]/ul/li[1]').click()
-        with allure.step('4、上传合同'):
-            with page.expect_file_chooser() as fc_info:
-                page.get_by_role("button", name="上传").click()
-            file_chooser = fc_info.value
-            file_chooser.set_files(self.pdf)
-        with allure.step('5、点击确定'):
+        if '姓名' in data:
+            with allure.step('输入姓名'):
+                page.get_by_role("dialog", name="添加合同").get_by_placeholder("请输入客户姓名").fill(data['姓名'])
+        if '手机号' in data:
+            with allure.step('输入手机号'):
+                page.get_by_role("dialog", name="添加合同").get_by_placeholder("请输入客户手机号").fill(data['手机号'])
+        if '合同编号' in data:
+            te = uuid.uuid4().hex[:9]
+            if data['合同编号'] == 'U':
+                data['合同编号'] = data['合同编号'] + te
+            elif data['合同编号'] == 'U1':
+                data['合同编号'] = data['合同编号'] + te + '123456789'
+            with allure.step('输入合同编号'):
+                page.get_by_role("dialog", name="添加合同").get_by_placeholder("请输入合同编号").fill(data['合同编号'])
+        if '学科' in data:
+            with allure.step('选择学科'):
+                page.get_by_role("dialog", name="添加合同").get_by_placeholder("请选择购买学科").click()
+                page.get_by_role("listitem").filter(has_text="Java").click()
+        if '课程' in data:
+            with allure.step('选择课程'):
+                page.get_by_role("dialog", name="添加合同").get_by_placeholder("请选择购买课程").click()
+                page.locator('xpath=/html/body/div[5]/div[1]/div[1]/ul/li[1]').click()
+
+        path = os.path.dirname(__file__)
+        if '合同' in data and not data['合同']:
+            pdf = path + os.sep + '1.pdf'
+            with allure.step('上传合同'):
+                with page.expect_file_chooser() as fc_info:
+                    page.get_by_role("button", name="上传").click()
+                file_chooser = fc_info.value
+                file_chooser.set_files(pdf)
+        elif '合同' in data and data['合同']:
+            pdf = path + os.sep + '1.text'
+            with allure.step('上传合同'):
+                with page.expect_file_chooser() as fc_info:
+                    page.get_by_role("button", name="上传").click()
+                file_chooser = fc_info.value
+                file_chooser.set_files(pdf)
+
+        if '渠道' in data:
+            with allure.step('选择渠道来源'):
+                page.get_by_role("textbox", name="请输入渠道来源").click()
+                page.get_by_role("listitem").filter(has_text="线上活动").click()
+        with allure.step('点击确定'):
             page.wait_for_timeout(2000)
             page.get_by_role("button", name="确 定").click()
-        with allure.step('6、断言'):
-            expect(page.locator("text=操作成功")).to_be_visible(timeout=2000)
-            expect(page.locator("text=操作成功")).to_contain_text('成功')
+
+        with allure.step('断言'):
+            expect(page.locator(x['a'])).to_contain_text(x['b'])
         # page.wait_for_timeout(2000)
-
-    @allure.title('添加合同失败（5位中文的姓名）')
-    def test_add2(self, login):
-        page = login
-        page.reload()
-
-        with allure.step('1、进入添加合同页面'):
-            page.get_by_role("button", name="添加合同").click()
-        with allure.step('2、输入手机号、合同编号、5位中文姓名'):
-            page.get_by_role("dialog", name="添加合同").get_by_placeholder("请输入客户姓名").fill('张三李四王')
-            page.get_by_role("dialog", name="添加合同").get_by_placeholder("请输入客户手机号").fill('13212341234')
-            page.get_by_role("dialog", name="添加合同").get_by_placeholder("请输入合同编号").fill(self.te)
-        with allure.step('3、选择学科与课程'):
-            page.get_by_role("dialog", name="添加合同").get_by_placeholder("请选择购买学科").click()
-            page.get_by_role("listitem").filter(has_text="Java").click()
-            page.get_by_role("dialog", name="添加合同").get_by_placeholder("请选择购买课程").click()
-            page.locator('xpath=/html/body/div[5]/div[1]/div[1]/ul/li[1]').click()
-        with allure.step('4、上传合同'):
-            with page.expect_file_chooser() as fc_info:
-                page.get_by_role("button", name="上传").click()
-            file_chooser = fc_info.value
-            file_chooser.set_files(self.pdf)
-        with allure.step('5、点击确定'):
-            page.wait_for_timeout(2000)
-            page.get_by_role("button", name="确 定").click()
-        with allure.step('6、断言'):
-            expect(page.locator("text=请输入真实的客户姓名")).to_be_visible(timeout=2000)
-            expect(page.locator("text=请输入真实的客户姓名")).to_contain_text('请输入真实的客户姓名')
-        # page.wait_for_timeout(2000)
-
-    @allure.title('添加合同失败（手机号非11位数字）')
-    def test_add3(self, login):
-        page = login
-        page.reload()
-
-        with allure.step('1、进入添加合同页面'):
-            page.get_by_role("button", name="添加合同").click()
-        with allure.step('2、输入姓名、合同编号、12位手机号'):
-            page.get_by_role("dialog", name="添加合同").get_by_placeholder("请输入客户姓名").fill('张三')
-            page.get_by_role("dialog", name="添加合同").get_by_placeholder("请输入客户手机号").fill('132123412345')
-            page.get_by_role("dialog", name="添加合同").get_by_placeholder("请输入合同编号").fill(self.te)
-        with allure.step('3、选择学科与课程'):
-            page.get_by_role("dialog", name="添加合同").get_by_placeholder("请选择购买学科").click()
-            page.get_by_role("listitem").filter(has_text="Java").click()
-            page.get_by_role("dialog", name="添加合同").get_by_placeholder("请选择购买课程").click()
-            page.locator('xpath=/html/body/div[5]/div[1]/div[1]/ul/li[1]').click()
-        with allure.step('4、上传合同'):
-            with page.expect_file_chooser() as fc_info:
-                page.get_by_role("button", name="上传").click()
-            file_chooser = fc_info.value
-            file_chooser.set_files(self.pdf)
-        with allure.step('5、点击确定'):
-            page.wait_for_timeout(2000)
-            page.get_by_role("button", name="确 定").click()
-        with allure.step('6、断言'):
-            expect(page.locator("text=请输入正确的客户手机号")).to_be_visible(timeout=2000)
-            expect(page.locator("text=请输入正确的客户手机号")).to_contain_text('请输入正确的客户手机号')
-        # page.wait_for_timeout(2000)
-
-    @allure.title('添加合同失败（合同编号为空）')
-    def test_add4(self, login):
-        page = login
-        page.reload()
-
-        with allure.step('1、进入添加合同页面'):
-            page.get_by_role("button", name="添加合同").click()
-        with allure.step('2、输入姓名、手机号'):
-            page.get_by_role("dialog", name="添加合同").get_by_placeholder("请输入客户姓名").fill('张三')
-            page.get_by_role("dialog", name="添加合同").get_by_placeholder("请输入客户手机号").fill('13212341234')
-        with allure.step('3、选择学科与课程'):
-            page.get_by_role("dialog", name="添加合同").get_by_placeholder("请选择购买学科").click()
-            page.get_by_role("listitem").filter(has_text="Java").click()
-            page.get_by_role("dialog", name="添加合同").get_by_placeholder("请选择购买课程").click()
-            page.locator('xpath=/html/body/div[5]/div[1]/div[1]/ul/li[1]').click()
-        with allure.step('4、上传合同'):
-            with page.expect_file_chooser() as fc_info:
-                page.get_by_role("button", name="上传").click()
-            file_chooser = fc_info.value
-            file_chooser.set_files(self.pdf)
-        with allure.step('5、点击确定'):
-            page.wait_for_timeout(2000)
-            page.get_by_role("button", name="确 定").click()
-        with allure.step('6、断言'):
-            expect(page.locator("text=合同编号不能为空")).to_be_visible(timeout=2000)
-            expect(page.locator("text=合同编号不能为空")).to_contain_text('合同编号不能为空')
-        # page.wait_for_timeout(2000)
-
-    @allure.title('添加合同失败（未上传合同）')
-    def test_add5(self, login):
-        page = login
-        page.reload()
-
-        with allure.step('1、进入添加合同页面'):
-            page.get_by_role("button", name="添加合同").click()
-        with allure.step('2、输入姓名、12位手机号、合同编号'):
-            page.get_by_role("dialog", name="添加合同").get_by_placeholder("请输入客户姓名").fill('张三')
-            page.get_by_role("dialog", name="添加合同").get_by_placeholder("请输入客户手机号").fill('13212341234')
-            page.get_by_role("dialog", name="添加合同").get_by_placeholder("请输入合同编号").fill(self.te)
-        with allure.step('3、选择学科与课程'):
-            page.get_by_role("dialog", name="添加合同").get_by_placeholder("请选择购买学科").click()
-            page.get_by_role("listitem").filter(has_text="Java").click()
-            page.get_by_role("dialog", name="添加合同").get_by_placeholder("请选择购买课程").click()
-            page.locator('xpath=/html/body/div[5]/div[1]/div[1]/ul/li[1]').click()
-
-        with allure.step('4、点击确定'):
-            page.wait_for_timeout(2000)
-            page.get_by_role("button", name="确 定").click()
-        with allure.step('5、断言'):
-            expect(page.locator("text=请上传合同")).to_be_visible(timeout=2000)
-            expect(page.locator("text=请上传合同")).to_contain_text('请上传合同')
-        page.wait_for_timeout(2000)
-
-if __name__ == '__main__':
-    pytest.main([__file__])
